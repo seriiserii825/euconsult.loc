d93c26a2b0d597911b0d822c45999487cb0224b4
serii <seriiburduja@gmail.com>
2019-08-30 15:42:16 +0300

    history

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,6 +17,24 @@ $(function () {
     fixedBackground: true
   });
 
+  let historySlider = function () {
+    $('#js-history-slider').slick({
+      arrows: false,
+      fade: true,
+      dots: true
+    });
+
+    $('.history__buttons .history-arow--left').on('click', function (e) {
+    	console.log($(this));
+      $('#js-history-slider').slick('slickNext');
+    });
+
+    $('.history__buttons .history-arow--right').on('click', function (e) {
+      $('#js-history-slider').slick('slickPrev');
+    });
+  };
+  historySlider();
+
 });
 
 
